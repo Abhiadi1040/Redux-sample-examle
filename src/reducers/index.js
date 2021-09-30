e499ac95d05107400c28f854970e590a5eb7e6da
Abhishek Kumar <abhishekadi1040@gmail.com>
2021-09-30 18:23:37 +0530

    first commit

---
+++ src/reducers/index.js
@@ -0,0 +1,14 @@
+import changeTheNumber from "./updown";
+
+import { combineReducers } from "redux";
+
+const reducers = combineReducers(
+    {
+    //   myNumber:changeTheNumber
+        changeTheNumber
+    }
+);
+
+export default reducers;
+
+//THIS FILE IS USED TO COMBINE REDUCERS 
